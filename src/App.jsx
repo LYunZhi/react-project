710a2b0db8e0356f09bc184778704315f4b4ed94
Jim <yjzlin@gmail.com>
2018-11-01 15:53:06 +0000

    Got rid of dummy data and removed commented out code

--- src/App.jsx
+++ src/App.jsx
@@ -2,22 +2,6 @@ import React, {Component} from 'react';
 import ChatBar from './ChatBar.jsx';
 import MessageList from './MessageList.jsx';
 
-const data = {
-  currentUser: {name: "Bob"}, // optional. if currentUser is not defined, it means the user is Anonymous
-  messages: [
-    {
-      id: 1,
-      username: "Bob",
-      content: "Has anyone seen my marbles?",
-    },
-    {
-      id: 2,
-      username: "Anonymous",
-      content: "No, I think you lost them. You lost your marbles Bob. You lost them for good."
-    }
-  ]
-}
-
 class App extends Component {
   constructor(props) {
     super(props)
